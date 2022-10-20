cf224146b070c2eb240c94019f0bfd90d7b8ab30
Akif Demirezen <ademirezen@thinksmobility.com>
2022-10-20 11:22:42 +0300

    DM-13600

---
+++ LocalizationFramework/OtiLocalization.swift
@@ -0,0 +1,22 @@
+//
+//  OtiLocalization.swift
+//  LocalizationFramework
+//
+//  Created by Akif Demirezen on 20.10.2022.
+//
+
+import Foundation
+//1
+extension String {
+    func localized(withComment comment: String = "") -> String {
+        return Bundle.main.localizedString(forKey: self,
+                                           value: "**\(self)**",
+                                           table: nil)
+    }
+}
+
+//2
+public enum Localization {
+    public static let title = "title".localized()
+    public static let description = "description".localized()
+}
