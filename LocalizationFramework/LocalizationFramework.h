1cca54e495753d51b8ba6334f72e84af14eec7bd
Akif Demirezen <ademirezen@thinksmobility.com>
2022-10-20 11:11:46 +0300

    Initial Commit

---
+++ LocalizationFramework/LocalizationFramework.h
@@ -0,0 +1,18 @@
+//
+//  LocalizationFramework.h
+//  LocalizationFramework
+//
+//  Created by Akif Demirezen on 20.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalizationFramework.
+FOUNDATION_EXPORT double LocalizationFrameworkVersionNumber;
+
+//! Project version string for LocalizationFramework.
+FOUNDATION_EXPORT const unsigned char LocalizationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalizationFramework/PublicHeader.h>
+
+
